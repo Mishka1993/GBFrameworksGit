e47c9531092b19fcf46622a48f47c0323317ac9b
Mikhail <misha220993@gmail.com>
2022-10-20 15:28:39 +0400

    init

---
+++ GBFrameworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBFrameworks
+//
+//  Created by Михаил Киржнер on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
